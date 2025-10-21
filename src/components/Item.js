5c585f06bd963ee26da3caadf27b956e84407592
Elly Omondi <owuorelly3@gmail.com>
2025-10-21 16:15:31 +0300

    Final commit

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  // Step 1: create state to track if item is in the cart
+  const [isInCart, setIsInCart] = useState(false);
+
+  // Step 2: handle button click
+  function handleAddToCartClick() {
+    // toggle the value each time button is clicked
+    setIsInCart((prevState) => !prevState);
+  }
+
+  // Step 3: dynamically set className and button text
+  const liClass = isInCart ? "in-cart" : "";
+  const buttonText = isInCart ? "Remove From Cart" : "Add to Cart";
+  const buttonClass = isInCart ? "remove" : "add";
+
   return (
-    <li className="">
+    <li className={liClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className={buttonClass} onClick={handleAddToCartClick}>
+        {buttonText}
+      </button>
     </li>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,19 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+  // Step 1: create state for selected category
+  const [selectedCategory, setSelectedCategory] = useState("All");
+
+  // Step 2: handle <select> change
+  function handleChange(event) {
+    setSelectedCategory(event.target.value);
+  }
+
+  // Step 3: filter items based on selected category
+  const itemsToDisplay = items.filter((item) => {
+    if (selectedCategory === "All") return true;
+    return item.category === selectedCategory;
+  });
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" value={selectedCategory} onChange={handleChange}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
           <option value="Dessert">Dessert</option>
         </select>
       </div>
+
       <ul className="Items">
-        {items.map((item) => (
+        {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
